b6e9cca9a8967c7d0f7f43c05a05e33d0b2160b7
Archit-KineticScape <archit@kineticscapestudios.com>
2024-12-21 21:32:16 +0530

    firebase and apis update

--- RightIntake/src/components/apis.jsx
+++ RightIntake/src/components/apis.jsx
@@ -1,8 +1,8 @@
-const baseurl = "http://147.93.18.134:3000/api/diet-plan";
+const baseurl = "https://api.rightintake.com/api/diet-plan";
 
 export const getfoodpreferences = `${baseurl}/generate-food-preferences`;
 export const saveUser = `${baseurl}/save-user`;
 export const generateInsights = `${baseurl}/generate-insights`;
 export const getCheatMeal = `${baseurl}/cheat-meal`;
-export const initiatePayment =`${baseurl}/initiate-payment`;
-export const verifyPayment =`${baseurl}/verify-payment`;
+export const initiatePayment = `${baseurl}/initiate-payment`;
+export const verifyPayment = `${baseurl}/verify-payment`;
