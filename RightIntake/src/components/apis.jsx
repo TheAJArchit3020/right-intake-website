63a92edb5ea3fa671ee6ddf7104a5d70494a7a0a
SumitSolapurkar21 <sumitsolapurkar21@gmail.com>
2025-03-26 16:27:08 +0530

    conatct us api added.

--- RightIntake/src/components/apis.jsx
+++ RightIntake/src/components/apis.jsx
@@ -1,4 +1,5 @@
-const baseurl = "https://api.rightintake.com/api/diet-plan";
+// const baseurl = "https://api.rightintake.com/api/diet-plan";
+const baseurl = "https://api.rightintake.com/api/app";
 
 export const getfoodpreferences = `${baseurl}/generate-food-preferences`;
 export const saveUser = `${baseurl}/save-temp`;
@@ -6,3 +7,4 @@ export const generateInsights = `${baseurl}/generate-insights`;
 export const getCheatMeal = `${baseurl}/cheat-meal`;
 export const initiatePayment = `${baseurl}/initiate-payment`;
 export const verifyPayment = `${baseurl}/verify-payment`;
+export const contactUs = `${baseurl}/contact`;
