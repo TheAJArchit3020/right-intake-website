e22a0dc3b71939f7726e7826360a97604ec70b63
Archit-KineticScape <archit@kineticscapestudios.com>
2025-03-27 13:32:55 +0530

    edited some basic stuff

--- RightIntake/src/components/navbarComponent/navbar.jsx
+++ RightIntake/src/components/navbarComponent/navbar.jsx
@@ -6,8 +6,7 @@ import { Link } from "react-router";
 import { useNavigate } from "react-router";
 import { fireemoji } from "../../components/Images";
 
-
-const NavbarComponent = ({scrollToSection}) => {
+const NavbarComponent = ({ scrollToSection }) => {
   const navigate = useNavigate();
   const [show, setShow] = useState(false);
 
@@ -18,64 +17,8 @@ const NavbarComponent = ({scrollToSection}) => {
     setShow(true);
   };
 
-
-
-
-
-
-
   return (
     <>
-      {/* <Navbar key={false} expand={false} className="bg-body mb-3">
-        <Container fluid>
-          <Navbar.Brand href="/">
-            <img src={realintakeslogo} alt="realintakeslogo" width={40} />
-            <span className="Rightintake-Logo-Name">Right Intake</span>
-          </Navbar.Brand>
-          <img
-            src={hammerimage}
-            alt="hammerimage"
-            width={40}
-            onClick={() => handleShow()}
-            className="hamburger-nav-bar"
-          />
-        </Container>
-      </Navbar>
-      <Offcanvas show={show} onHide={handleClose} placement="end">
-        <Offcanvas.Header closeButton></Offcanvas.Header>
-        <Offcanvas.Body className="sidebar-menu d-flex flex-column gap-4 ">
-          <Link to={"/"}>
-            <div className="home-btn">
-              <span className="Heading2">Home</span>
-            </div>
-          </Link>
-          <Link to={"/termsandservices"}>
-            <div className="terms-a-c-btn">
-              <span className="Heading2">Terms and Services</span>
-            </div>
-          </Link>
-          <Link to={"/contactus"}>
-            <div className="contact-us-btn">
-              <span className="Heading2">Contact Us</span>
-            </div>
-          </Link>
-          <Link to={"/aboutus"}>
-            <div className="about-us-btn">
-              <span className="Heading2">About Us</span>
-            </div>
-          </Link>
-          <div
-            className="nav-bar-cta mt-5"
-            onClick={() => navigate("/dietplanform")}
-          >
-            <span className="nav-bar-btn-Text">
-              Get your personalized plan now
-            </span>
-            <img src={fireemoji} alt="" />
-          </div>
-        </Offcanvas.Body>
-      </Offcanvas> */}
-
       {/* new code */}
       <div className="newNavbarContainer">
         <div className="navBarBrand">
@@ -83,12 +26,36 @@ const NavbarComponent = ({scrollToSection}) => {
           <span className="Rightintake-Logo-Name">Right Intake</span>
         </div>
         <div className="navTabs">
-          <p className="navTabs-p" onClick={()=> scrollToSection('home')} >Home</p>
-          <p className="navTabs-p" onClick={()=> scrollToSection('tracking')}>Tracking</p>
-          <p className="navTabs-p" onClick={()=> scrollToSection('foodlens')}>FoodLens</p>
-          <p className="navTabs-p" onClick={()=> scrollToSection('smartmeal')}>Smartmeal</p>
-          <p className="navTabs-p" onClick={()=> scrollToSection('nutriplans')}>NutriPlans</p>
-          <p className="navTabs-p" onClick={()=> scrollToSection('reviews')}>Reviews</p>
+          <div className="navTabs-p" onClick={() => scrollToSection("home")}>
+            <span>Home</span>
+          </div>
+          <div
+            className="navTabs-p"
+            onClick={() => scrollToSection("tracking")}
+          >
+            <span>Tracking</span>
+          </div>
+          <div
+            className="navTabs-p"
+            onClick={() => scrollToSection("foodlens")}
+          >
+            <span>FoodLens</span>
+          </div>
+          <div
+            className="navTabs-p"
+            onClick={() => scrollToSection("smartmeal")}
+          >
+            <span>Smartmeal</span>
+          </div>
+          <div
+            className="navTabs-p"
+            onClick={() => scrollToSection("nutriplans")}
+          >
+            <span>NutriPlans</span>
+          </div>
+          <div className="navTabs-p" onClick={() => scrollToSection("reviews")}>
+            <span>Reviews</span>
+          </div>
         </div>
         <div className="getappbutton">
           <p className="getappbutton-p">Get rightintake app</p>
